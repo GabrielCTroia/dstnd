9d8b41e1b78d09c0d2839b7907c45767e9fb3c2f
Gabriel C. Troia <catalin.troia@gmail.com>
2020-04-08 15:57:03 +0300

    feat(webrtc): add the WEBRTC module succesfully

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebRTCInit } from 'src/services/webrtc';
+import { s } from 'src/services/s';
+
+
+// console.log(WebRTCInit);
 
 export default function App() {
+  useEffect(() => {
+    s();
+    WebRTCInit();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app works2!</Text>
+      <Text>Open up App.tsx to start working on your app works!</Text>
     </View>
   );
 }
---
+++ src/services/s.ts
@@ -0,0 +1,3 @@
+export const s = () => {
+  console.log('attat');
+}
